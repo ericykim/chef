137c7a2d3785916054d142141869f35bb1a36087
gvjacob <gvjacob@outlook.com>
2019-06-16 15:15:52 -0400

    api module

---
+++ server/src/api/api.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import PostgresModule from '../postgres/postgres.module';
+import Recipe from '../postgres/entities/recipe.entity';
+import RecipeController from './controllers/recipe.controller';
+import Label from '../postgres/entities/label.entity';
+
+@Module({
+  imports: [PostgresModule.forRoot([Recipe, Label])],
+  controllers: [RecipeController],
+})
+export class ApiModule {}
+
+export default ApiModule;
