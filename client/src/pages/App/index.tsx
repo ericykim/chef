077b36508d5919092a3247c96b87924195caeab0
gvjacob <gvjacob@outlook.com>
2019-06-22 22:23:51 -0400

    Login page

--- client/src/pages/App/index.tsx
+++ client/src/pages/App/index.tsx
@@ -4,19 +4,12 @@ import styles from './styles.css';
 import wretch from 'wretch';
 import Recipe from '../../components/Recipe';
 import RecipeType from '../../entities/Recipe';
+import Login from '../Login';
 
 const App: FunctionComponent = (props) => {
-  const [recipe, setRecipe] = useState<RecipeType>({} as RecipeType);
-
-  useEffect(() => {
-    wretch('http://localhost:3000/recipe/f5b25ae8-4ec8-4656-ba03-3a92024bd278')
-      .get()
-      .json((json) => setRecipe(json as RecipeType));
-  }, []);
-
   return (
     <div>
-      <Recipe recipe={recipe} />
+      <Login />
     </div>
   );
 };
