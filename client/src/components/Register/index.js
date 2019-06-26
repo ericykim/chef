835d74156aa236d34894162dd64a6971f5db6726
gvjacob <gvjacob@outlook.com>
2019-06-25 20:17:38 -0400

    Register component

--- client/src/components/Register/index.js
+++ client/src/components/Register/index.js
@@ -1,8 +1,149 @@
-import React from 'react';
+import React, { useState } from 'react';
+import wretch from 'wretch';
+
+import { Form, Icon, Checkbox, Card, Input, Button } from 'antd';
 import styles from './styles.css';
 
-const Register = (props) => {
-  return <div />;
+/**
+ * Register widget for creating a new account.
+ */
+const Register = ({ className, form }) => {
+  const [error, setError] = useState('');
+  const { validateFields, getFieldDecorator } = form;
+
+  const register = (e) => {
+    e.preventDefault();
+
+    validateFields(async (err, values) => {
+      if (!err) {
+        await wretch('http://localhost:3000/auth/register')
+          .post(values)
+          .error(409, (error) => setError('Username or email is taken'))
+          .res(() => setError(''))
+          .catch(() => setError('Something went wrong'));
+      }
+    });
+  };
+
+  const comparePasswords = (rule, value, callback) => {
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Passwords do not match');
+    } else {
+      callback();
+    }
+  };
+
+  return (
+    <Card
+      className={className}
+      title={'Create Account'}
+      extra={<span className={styles.error}>{error}</span>}
+      data-testid={'Register'}
+    >
+      <Form onSubmit={register}>
+        <div className={styles.names}>
+          <div className={styles.firstName}>
+            <Form.Item>
+              {getFieldDecorator('firstName', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'First name is required',
+                  },
+                ],
+              })(<Input placeholder="First name" size={'large'} />)}
+            </Form.Item>
+          </div>
+
+          <div className={styles.lastName}>
+            <Form.Item>
+              {getFieldDecorator('lastName', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Last name is required',
+                  },
+                ],
+              })(
+                <Input
+                  className={styles.lastName}
+                  placeholder="Last name"
+                  size={'large'}
+                />,
+              )}
+            </Form.Item>
+          </div>
+        </div>
+
+        <Form.Item>
+          {getFieldDecorator('email', {
+            rules: [
+              {
+                required: true,
+                message: 'Email address is required',
+              },
+              {
+                type: 'email',
+                message: 'Invalid email address',
+              },
+            ],
+          })(<Input placeholder="Email address" size={'large'} />)}
+        </Form.Item>
+
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [
+              {
+                required: true,
+                message: 'Username is required',
+              },
+            ],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="Username"
+              size={'large'}
+            />,
+          )}
+        </Form.Item>
+
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Password is required' }],
+          })(
+            <Input.Password
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Password"
+              size={'large'}
+            />,
+          )}
+        </Form.Item>
+
+        <Form.Item>
+          {getFieldDecorator('confirmPassword', {
+            rules: [
+              { required: true, message: 'Confirm your password' },
+              { validator: comparePasswords },
+            ],
+          })(
+            <Input.Password
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="Confirm Password"
+              size={'large'}
+            />,
+          )}
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className={styles.register}>
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
 };
 
-export default Register;
+export default Form.create()(Register);
--- client/src/components/Register/test.js
+++ client/src/components/Register/test.js
@@ -1,1 +1,142 @@
+import React from 'react';
+import { render, cleanup, fireEvent, wait } from '@testing-library/react';
+import { FetchMock } from '@react-mock/fetch';
+import v4 from 'uuid/v4';
 
+import Register from '.';
+
+describe('Register', () => {
+  describe('successful registration', () => {});
+
+  describe('failing registration', () => {
+    const conflictMock = {
+      matcher: 'http://localhost:3000/auth/register',
+      method: 'POST',
+      response: { status: 409, body: {} },
+    };
+
+    const errorMock = {
+      matcher: 'http://localhost:3000/auth/register',
+      method: 'POST',
+      response: { status: 500, body: {} },
+    };
+
+    it('notifies user of missing fields', async () => {
+      const { container, getByText } = render(<Register />);
+      fireEvent.click(container.querySelector('button[type="submit"]'));
+
+      await wait(() => {
+        expect(getByText('First name is required')).toBeInTheDocument();
+        expect(getByText('Last name is required')).toBeInTheDocument();
+        expect(getByText('Email address is required')).toBeInTheDocument();
+        expect(getByText('Username is required')).toBeInTheDocument();
+        expect(getByText('Password is required')).toBeInTheDocument();
+        expect(getByText('Confirm your password')).toBeInTheDocument();
+      });
+    });
+
+    it('prompts to enter valid email', async () => {
+      const { getByPlaceholderText, getByText } = render(<Register />);
+
+      fireEvent.change(getByPlaceholderText('Email address'), {
+        target: { value: 'gramsey' },
+      });
+
+      await wait(() => {
+        expect(getByText('Invalid email address')).toBeInTheDocument();
+      });
+    });
+
+    it('prompts to confirm password correctly', async () => {
+      const { getByPlaceholderText, getByText } = render(<Register />);
+
+      fireEvent.change(getByPlaceholderText('Password'), {
+        target: { value: 'secret sauce' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Confirm Password'), {
+        target: { value: 'is' },
+      });
+
+      await wait(() => {
+        expect(getByText('Passwords do not match')).toBeInTheDocument();
+      });
+    });
+
+    it('notifies user of account conflict', async () => {
+      const { container, getByPlaceholderText, getByText } = render(
+        <FetchMock {...conflictMock}>
+          <Register />,
+        </FetchMock>,
+      );
+
+      fireEvent.change(getByPlaceholderText('First name'), {
+        target: { value: 'Gordon' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Last name'), {
+        target: { value: 'Ramsey' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Email address'), {
+        target: { value: 'gramsey@hell.com' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Username'), {
+        target: { value: 'idiotsandwich' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Password'), {
+        target: { value: 'I am secretly weak' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Confirm Password'), {
+        target: { value: 'I am secretly weak' },
+      });
+
+      fireEvent.click(container.querySelector('button[type="submit"]'));
+
+      await wait(() => {
+        expect(getByText('Username or email is taken')).toBeInTheDocument();
+      });
+    });
+
+    it('notifies user of unexpected error', async () => {
+      const { container, getByPlaceholderText, getByText } = render(
+        <FetchMock {...errorMock}>
+          <Register />,
+        </FetchMock>,
+      );
+
+      fireEvent.change(getByPlaceholderText('First name'), {
+        target: { value: 'Gordon' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Last name'), {
+        target: { value: 'Ramsey' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Email address'), {
+        target: { value: 'gramsey@hell.com' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Username'), {
+        target: { value: 'idiotsandwich' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Password'), {
+        target: { value: 'I am secretly weak' },
+      });
+
+      fireEvent.change(getByPlaceholderText('Confirm Password'), {
+        target: { value: 'I am secretly weak' },
+      });
+
+      fireEvent.click(container.querySelector('button[type="submit"]'));
+
+      await wait(() => {
+        expect(getByText('Something went wrong')).toBeInTheDocument();
+      });
+    });
+  });
+});
