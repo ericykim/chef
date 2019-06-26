324ce84b3559d1cc813e831705d46e354e36b7a5
gvjacob <gvjacob@outlook.com>
2019-06-25 20:16:36 -0400

    remove typescript

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,14 +1,18 @@
 module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['js', 'jsx'],
   verbose: true,
   transform: {
-    '^.+\\.js?$': 'babel-jest',
+    '^.+\\.jsx?$': 'babel-jest',
   },
-  // moduleNameMapper: {
-  //   '\\.css$': '<rootDir>/__mocks__/styleMock.js',
-  // },
+  moduleNameMapper: {
+    '\\.css$': '<rootDir>/__mocks__/styleMock.js',
+  },
+  testMatch: ['/**/*test.js'],
   collectCoverage: false,
-  // collectCoverageFrom: ['./src/**/*.js', '!**/node_modules/**'],
+  collectCoverageFrom: ['./src/**/*.js', '!**/node_modules/**'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  testEnvironment: 'jsdom',
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
