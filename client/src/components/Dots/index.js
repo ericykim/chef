4a67bea36c6ab461e9dfb5cbb9eb888967ba192d
gvjacob <gvjacob@outlook.com>
2019-07-03 08:43:42 -0400

    Navigation arrows for dots

--- client/src/components/Dots/index.js
+++ client/src/components/Dots/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Button, Icon } from 'antd';
 import cn from 'classnames';
 
 import styles from './styles.css';
@@ -7,16 +8,39 @@ import styles from './styles.css';
  * Dots for scrolling through array.
  */
 const Dots = ({ className, elements, selected, onChange }) => {
+  const leftDisable = selected === 0;
+  const rightDisable = selected === elements.length - 1;
+
   return (
-    <div className={cn(styles.dots, className)} data-testid={'Dots'}>
-      {elements.map((_, index) => (
-        <button
-          key={index}
-          className={cn(styles.dot, { [styles.selected]: index === selected })}
-          onClick={() => onChange(index)}
-          data-testid={`dot-${index}`}
-        />
-      ))}
+    <div className={cn(styles.container, className)} data-testid={'Dots'}>
+      <Button
+        size={'small'}
+        type={'link'}
+        icon={'left'}
+        disabled={leftDisable}
+        onClick={() => onChange(selected - 1)}
+        data-testid={'left'}
+      />
+      <div className={styles.dots}>
+        {elements.map((_, index) => (
+          <button
+            key={index}
+            className={cn(styles.dot, {
+              [styles.selected]: index === selected,
+            })}
+            onClick={() => onChange(index)}
+            data-testid={`dot-${index}`}
+          />
+        ))}
+      </div>
+      <Button
+        size={'small'}
+        type={'link'}
+        icon={'right'}
+        onClick={() => onChange(selected + 1)}
+        disabled={rightDisable}
+        data-testid={'right'}
+      />
     </div>
   );
 };
--- client/src/components/Dots/test.js
+++ client/src/components/Dots/test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
 
 import Dots from '.';
 
 describe('Dots', () => {
-  let wrapper;
   let elements;
   let selected;
   let onChangeMock;
@@ -13,19 +12,22 @@ describe('Dots', () => {
     elements = [1, 2, 3];
     selected = 0;
     onChangeMock = jest.fn();
-
-    wrapper = render(
-      <Dots elements={elements} selected={selected} onChange={onChangeMock} />,
-    );
   });
 
+  afterEach(cleanup);
+
   it('renders', () => {
-    const { getByTestId } = wrapper;
+    const { getByTestId } = render(
+      <Dots elements={elements} selected={selected} onChange={onChangeMock} />,
+    );
+
     expect(getByTestId('Dots')).toBeInTheDocument();
   });
 
   it('renders as many dots', () => {
-    const { getAllByTestId } = wrapper;
+    const { getAllByTestId } = render(
+      <Dots elements={elements} selected={selected} onChange={onChangeMock} />,
+    );
 
     expect(getAllByTestId('dot-', { exact: false }).length).toBe(
       elements.length,
@@ -33,9 +35,65 @@ describe('Dots', () => {
   });
 
   it('calls onChange when clicking on a dot', () => {
-    const { getByTestId } = wrapper;
+    const { getByTestId } = render(
+      <Dots elements={elements} selected={selected} onChange={onChangeMock} />,
+    );
     fireEvent.click(getByTestId('dot-2'));
 
     expect(onChangeMock).toHaveBeenCalledWith(2);
+  });
+
+  it('enabled both buttons on middle dots', () => {
+    const { getByTestId } = render(
+      <Dots elements={elements} selected={1} onChange={onChangeMock} />,
+    );
+
+    const left = getByTestId('left');
+    const right = getByTestId('right');
+
+    expect(left).not.toHaveAttribute('disabled');
+    expect(right).not.toHaveAttribute('disabled');
+  });
+
+  it('disables left button on first dot', () => {
+    const { getByTestId } = render(
+      <Dots elements={elements} selected={selected} onChange={onChangeMock} />,
+    );
+
+    const left = getByTestId('left');
+    fireEvent.click(left);
+
+    expect(left).toHaveAttribute('disabled');
+    expect(onChangeMock).not.toHaveBeenCalled();
+  });
+
+  it('increments step on right', () => {
+    const { getByTestId } = render(
+      <Dots elements={elements} selected={selected} onChange={onChangeMock} />,
+    );
+    fireEvent.click(getByTestId('right'));
+
+    expect(onChangeMock).toHaveBeenCalledWith(selected + 1);
+  });
+
+  it('decrements step on left', () => {
+    const { getByTestId } = render(
+      <Dots elements={elements} selected={1} onChange={onChangeMock} />,
+    );
+
+    fireEvent.click(getByTestId('left'));
+    expect(onChangeMock).toHaveBeenCalledWith(0);
+  });
+
+  it('disables right button on last dot', () => {
+    const { getByTestId } = render(
+      <Dots elements={elements} selected={2} onChange={onChangeMock} />,
+    );
+
+    const right = getByTestId('right');
+    fireEvent.click(right);
+
+    expect(right).toHaveAttribute('disabled');
+    expect(onChangeMock).not.toHaveBeenCalled();
   });
 });
