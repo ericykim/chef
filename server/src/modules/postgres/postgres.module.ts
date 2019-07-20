91ee0fe27f536818cdde66bb4e836241887af51a
gvjacob <gvjacob@outlook.com>
2019-07-20 19:10:31 -0400

    Docker containers for all services

--- server/src/modules/postgres/postgres.module.ts
+++ server/src/modules/postgres/postgres.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import Recipe from './entities/recipe.entity';
-import Label from './entities/label.entity';
-import Chef from './entities/chef.entity';
+const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB } = process.env;
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: DB_HOST,
       port: 5432,
-      username: 'chef',
-      password: 'i am chef',
-      database: 'chef',
+      username: DB_USERNAME,
+      password: DB_PASSWORD,
+      database: DB,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
       uuidExtension: 'pgcrypto',
