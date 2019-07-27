5be3f0ebaad17d6214e9a6c251938855a80db0b8
gvjacob <gvjacob@outlook.com>
2019-07-27 00:34:28 -0400

    initialize extension

--- postgres/scripts/01-init.sql
+++ postgres/scripts/01-init.sql
@@ -4,6 +4,9 @@ CREATE USER postgres WITH ENCRYPTED PASSWORD 'postgres';
 -- Integration test database
 CREATE DATABASE integration_test;
 
+-- UUID creation extension
+CREATE EXTENSION pgcrypto;
+
 -- Grant superuser privileges to postgres user.
 ALTER USER postgres WITH SUPERUSER;
 GRANT ALL PRIVILEGES ON DATABASE integration_test TO chef;
