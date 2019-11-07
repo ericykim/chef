a66db87b62e05bd2ae99b4765e336b25545821b8
gvjacob <gvjacob@outlook.com>
2019-11-06 22:27:24 -0500

    next and prev buttons

--- src/pages/RecipeWalkthrough/index.js
+++ src/pages/RecipeWalkthrough/index.js
@@ -10,7 +10,7 @@ const RecipeWalkthrough = ({ className, match, history, setDocumentTitle }) => {
     params: { id },
   } = match;
   const { findRecipe } = useContext(UserContext);
-  const { directions } = findRecipe(id);
+  const { title, directions } = findRecipe(id);
 
   const [index, setIndex] = useState(0);
 
@@ -29,17 +29,43 @@ const RecipeWalkthrough = ({ className, match, history, setDocumentTitle }) => {
   const step = index + 1;
 
   return (
-    <div>
-      {step + '. ' + directions[index]}
+    <div className={styles.page}>
       <div>
-        <span>
-          <Button type='primary' onClick={handlePreviousStep}>
+        <div className={styles.following}>Following</div>
+        <h1>{title}</h1>
+      </div>
+
+      <div className={styles.step}>
+        <b>Current: </b>
+        <h2>{`${step}. ${directions[index]}`}</h2>
+      </div>
+
+      {index < directions.length - 1 && (
+        <div className={styles.nextStep}>
+          <b>Next: </b>
+          <h2>{`${step + 1}. ${directions[index + 1]}`}</h2>
+        </div>
+      )}
+
+      <div className={styles.buttons}>
+        {step === 1 ? (
+          <Button type="primary" onClick={() => history.push('/profile')}>
+            Quit
+          </Button>
+        ) : (
+          <Button type="primary" onClick={handlePreviousStep}>
             Back
           </Button>
-          <Button type='primary' onClick={handleNextStep}>
+        )}
+        {step === directions.length ? (
+          <Button type="primary" onClick={() => history.push('/profile')}>
+            Finish
+          </Button>
+        ) : (
+          <Button type="primary" onClick={handleNextStep}>
             Next
           </Button>
-        </span>
+        )}
       </div>
     </div>
   );
